03c18e14aa7b220219a404219019474b8d9e219d
Roberto Vega <roberto.vega87@gmail.com>
2016-05-31 01:16:57 +0000

    update the converter to actually do something and display the conversion. also wanted to toy with modularity of Angular being able to snap together a directive and add it to the application

--- js/asciiConvert.js
+++ js/asciiConvert.js
@@ -1,6 +1,6 @@
 (function(){
     'use-strict'
-	var asciiConvert = angular.module("asciiDirective",[])
+    var asciiConvert = angular.module("asciiDirective",[])
     .factory('asciiFactory',['$http',function($http){
         return{
             get:function(){
@@ -16,8 +16,23 @@
         }
     }])
     .controller('asciiConvertController',['$scope','asciiFactory',function($scope,asciiFactory){
-     
-        
+        $scope.asciiArray=[];
+        $scope.charArray=[]
+        $scope.asciiSpliceString=function(str){
+            var seperator = /\,/;
+            $scope.asciiArray = str.split(seperator);
+            $scope.charArray = [];
+            for(var i=0; i<$scope.asciiArray.length;i++){
+                $scope.charArray.push(String.fromCharCode($scope.asciiArray[i]))
+
+                console.log(String.fromCharCode($scope.asciiArray[i]))
+            }
+            console.log($scope.charArray)
+            console.log( $scope.asciiArray);
+            $scope.finalString= $scope.charArray.join('');
+            document.getElementById("messageConverted").innerHTML = $scope.finalString;
+        }
+
     }]).directive('asciiConvertCard',function(){
         function link (scope, element, attrs){
 
